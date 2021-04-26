5b066f31babccde8c68d99d257f9af3280e73f11
jensnti <jensandreasson77@gmail.com>
2021-04-26 20:48:02 +0200

    chore: api cfg

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@ const { i18n } = require('./next-i18next.config');
 
 module.exports = {
   env: {
-    apiUrl: 'http://localhost:8080/api'
-    // apiUrl: 'https://desolate-chamber-36984.herokuapp.com/api'
+    // apiUrl: 'http://localhost:8080/api'
+    apiUrl: 'https://desolate-chamber-36984.herokuapp.com/api'
   },
   i18n
 };
