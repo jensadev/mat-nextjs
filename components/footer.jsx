c9fdf329ec91ef47f9f6f673809677433ccc7c6a
jensnti <jensandreasson77@gmail.com>
2021-05-06 21:13:55 +0200

    feat: footer attribution

--- components/footer.jsx
+++ components/footer.jsx
@@ -10,30 +10,87 @@ export default function Footer() {
 
     return (
         <footer className="footer mt-auto">
-            <div className="row-calc container d-flex justify-content-between align-items-center pt-3 pb-5">
-                <div>
-                    {router.locale !== 'en' ? (
-                        <Link href={router.pathname} locale="en">
-                            <a className="link-footer">en</a>
-                        </Link>
-                    ) : (
-                        <span className="link-footer active">en</span>
-                    )}
-                    <span className="px-1 text-muted h3">|</span>
-                    {router.locale !== 'sv' ? (
-                        <Link href={router.pathname} locale="sv">
-                            <a className="link-footer">sv</a>
-                        </Link>
-                    ) : (
-                        <span className="link-footer active">sv</span>
-                    )}
+            <div className="row-calc">
+                <div className="container pt-5">
+                    <div className="row">
+                        <div className="col d-flex align-items-center justify-content-between">
+                            <div>
+                                {router.locale !== 'en' ? (
+                                    <Link href={router.pathname} locale="en">
+                                        <a className="link-footer">en</a>
+                                    </Link>
+                                ) : (
+                                    <span className="link-footer active">
+                                        en
+                                    </span>
+                                )}
+                                <span className="px-1 text-muted h3">|</span>
+                                {router.locale !== 'sv' ? (
+                                    <Link href={router.pathname} locale="sv">
+                                        <a className="link-footer">sv</a>
+                                    </Link>
+                                ) : (
+                                    <span className="link-footer active">
+                                        sv
+                                    </span>
+                                )}
+                            </div>
+                            <Link href="/legal">
+                                <a className="link-footer">
+                                    {`${t('common:terms')}`}
+                                    {/* {`${t('cookie')} ${t('policy')} / ${t('privacy')} ${t('policy')}`} */}
+                                </a>
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="row pt-4">
+                        <div className="col">
+                            <p className="text-small text-muted">
+                                {t('common:created_by')}{' '}
+                                <a
+                                    href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#106;&#101;&#110;&#115;&#97;&#110;&#100;&#114;&#101;&#97;&#115;&#115;&#111;&#110;&#55;&#55;&#64;&#103;&#109;&#97;&#105;&#108;&#46;&#99;&#111;&#109;"
+                                    rel="noreferrer">
+                                    &#74;&#101;&#110;&#115;&#32;&#65;&#110;&#100;&#114;&#101;&#97;&#115;&#115;&#111;&#110;
+                                </a>{' '}
+                                {t('common:using')}{' '}
+                                <a
+                                    target="_blank"
+                                    href="https://nextjs.org/"
+                                    rel="noreferrer">
+                                    Next.js
+                                </a>{' '}
+                                {t('common:deployed_with')}{' '}
+                                <a
+                                    target="_blank"
+                                    href="https://vercel.com/"
+                                    rel="noreferrer">
+                                    Vercel
+                                </a>
+                                .
+                            </p>
+                            <p className="text-small text-muted">
+                                {t('common:source_code')}{' '}
+                                <a
+                                    target="_blank"
+                                    href="https://github.com/jensnti/mat-nextjs"
+                                    rel="noreferrer">
+                                    GitHub
+                                </a>
+                                .
+                            </p>
+                            <p className="text-small text-muted">
+                                {t('common:found_a_bug')}{' '}
+                                <a
+                                    target="_blank"
+                                    href="https://github.com/jensnti/mat-nextjs/issues"
+                                    rel="noreferrer">
+                                    {t('common:issue')}
+                                </a>
+                                .
+                            </p>
+                        </div>
+                    </div>
                 </div>
-                <Link href="/legal">
-                    <a className="link-footer">
-                        {`${t('common:terms')}`}
-                        {/* {`${t('cookie')} ${t('policy')} / ${t('privacy')} ${t('policy')}`} */}
-                    </a>
-                </Link>
             </div>
         </footer>
     );
